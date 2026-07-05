ade2b7e41b78008e1c53b77f0bccd2816387d7fd
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-07-05 16:47:06 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,1 +1,18 @@
-// Auto-generated stub
+// FILE: svelte.config.js
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
+  }
+};
+
+export default config;
